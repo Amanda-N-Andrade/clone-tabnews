e9c792e84190429fa9b5c512a74340f46dd27c64
Amanda Andrade <86500009+Amanda-N-Andrade@users.noreply.github.com>
2023-10-06 03:48:46 +0000

    Add scripts `lint:check` and `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
-function Hello(){
-    return <h1>O medo mora perto das idéias loucas!</h1>
+function Hello() {
+  return <h1>O medo mora perto das idéias loucas!</h1>;
 }
 
+function Teste() {
+  console.log("Prettier Test.");
+}
 export default Hello;
